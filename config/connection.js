c46f1b6ea84970b40bfe266b70f46da6e19e58a0
Todd Germaine <trgermaine@gmail.com>
2016-08-18 19:15:02 -0400

    updated schema

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
 	host: 'localhost',
 	user: 'root',
 	password: '',
-	database: 'quests_db4'
+	database: 'quests_db3'
 });
 
 connection.connect(function (err) {
